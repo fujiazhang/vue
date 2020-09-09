8179421e16c36ca636a37d05ae5a6c86d4b719d6
fujia_zhang <i@iamzfj.cn>
2020-09-09 20:52:01 +0800

    下班下班

---
+++ src/observer.js
@@ -0,0 +1,14 @@
+
+/**
+ * @description: 
+ * 1.负责将data选项中属性转换成响应式数据
+ * 2.data中的某个属性也是对象时，一样换成响应式数据
+ * 3.数据变化时 发送通知
+ * @param {type} 
+ * @return {type} 
+ */
+class Observer {
+  walk(data) {
+
+  }
+}
---
+++ src/vue.js
@@ -0,0 +1,31 @@
+class Vue {
+  constructor(options) {
+    // 1.通过属性保存数据
+    this.$options = options || {}
+    this.$data = options.data || {}
+    this.$el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el
+    // 2.把data中的成员转换成getter setter 注入到实例
+    this._proxyData(this.$data)
+    // 3.跳动observer对象，监听数据变化
+    // 4.调用compiler,解析指令和差值表达式
+  }
+  _proxyData(data) {
+    //遍历所有属性
+    Object.keys(data).forEach(key => {
+      //将data注入vue实例
+      Object.defineProperty(this, key, {
+        enumerable: true,
+        configurable: true,
+        get() {
+          return data[key]
+        },
+        set(newValue) {
+          if (newValue !== data[key]) {
+            data[key] = newValue
+          }
+        }
+      })
+    })
+
+  }
+}
